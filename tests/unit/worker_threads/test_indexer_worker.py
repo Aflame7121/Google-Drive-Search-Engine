e0a3ffd837fc1767ca19d17cadcdba56a2a669db
gapcomputer <gapcomputer@users.noreply.github.com>
2025-05-23 21:00:40 +0000

    Update IndexerWorker test to be more import-friendly

--- tests/unit/worker_threads/test_indexer_worker.py
+++ tests/unit/worker_threads/test_indexer_worker.py
@@ -1,13 +1,20 @@
 import pytest
-from WorkerThreads.IndexerWorker import IndexerWorker
+import sys
+import os
 
-class TestIndexerWorker:
-    def test_indexer_worker_initialization(self):
-        """Test basic initialization of IndexerWorker."""
-        worker = IndexerWorker()
-        assert worker is not None
-        
-    def test_indexer_worker_interface(self):
-        """Verify basic interface methods exist."""
-        worker = IndexerWorker()
-        assert hasattr(worker, 'run'), "IndexerWorker should have a run method"
+# Add project root to Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+
+def test_indexer_worker_import():
+    """Test ability to import IndexerWorker."""
+    try:
+        from WorkerThreads.IndexerWorker import IndexerWorker
+        assert True
+    except ImportError:
+        pytest.fail("Could not import IndexerWorker")
+
+def test_indexer_worker_attributes():
+    """Verify basic attributes of IndexerWorker."""
+    from WorkerThreads.IndexerWorker import IndexerWorker
+    worker = IndexerWorker()
+    assert hasattr(worker, 'run'), "IndexerWorker should have a run method"
